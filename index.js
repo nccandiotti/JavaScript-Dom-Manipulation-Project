7d56738412230e117726a44fd2c6600eb37edca2
Elizabeth Catoe <eccatoe2517@gmail.com>
2022-01-24 16:18:29 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+
+//create nav bar that filters tea and coffee
+
+//Fetch Coffees
+//render Images into menu container
+//add click event to coffee cards that:
+//render image with form to drink display
+//a form with editable size, flavor
+//an add to cart button
+
+
+
+//add to cart button will: 
+//add list item to cart with:
+//price that will update
+//name of drink
+//delete button
+
+
+//create a drink form with:
+//base drink
+//flavor dropdown
+//size dropdown
+//name input?
+
+
+
