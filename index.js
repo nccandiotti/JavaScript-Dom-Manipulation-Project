69fc572596df3e786f41d4cbaac1f3e76d123fde
Elizabeth Catoe <eccatoe2517@gmail.com>
2022-01-24 17:17:53 -0500

    New CSS stuff/ plain english MVPs

--- index.js
+++ index.js
@@ -1,28 +1,35 @@
 
-//create nav bar that filters tea and coffee
+
 
 //Fetch Coffees
-//render Images into menu container
+//render Images into menu container with names
 //add click event to coffee cards that:
-//render image with form to drink display
+//render image, name, form to drink display
 //a form with editable size, flavor
+//also default values for coffee selected, size, flavor
+//price
 //an add to cart button
 
 
 
 //add to cart button will: 
-//add list item to cart with:
-//price that will update
+//add list item to cart container with:
+//price that will update on bottom bar as drinks are added
 //name of drink
 //delete button
 
 
 //create a drink form with:
+//default values for all inputs
 //base drink
 //flavor dropdown
 //size dropdown
-//name input?
+//name input-will replace drink name when posted to db.json
 
-
-
-sfghljkghflajk
+//STRETCH
+//populate nav bar with filter option for coffee/tea
+//add comment section (reviews for shop)
+//add star rating?
+//change CSS featues based on selected flavor
+//reflect price when flavors and size are changed
+//
